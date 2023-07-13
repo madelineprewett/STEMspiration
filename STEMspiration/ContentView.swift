728f3f1b96633add4b77c31a703ac441586b5e2e
Madeline Prewett <madsprewett@gmail.com>
2023-07-13 09:58:38 -0700

    Initial Commit

---
+++ STEMspiration/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  STEMspiration
+//
+//  Created by scholar on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack {
+            Color(.systemBrown)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("janeandmonkey")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                .cornerRadius(15)
+                
+                HStack {
+                    
+                    Text("Jane Goodall")
+                        .font(.title)
+                    .fontWeight(.bold)
+                    Text("Chimpanzee Lady")
+                        .fontWeight(.bold)
+                    
+                }
+                
+                
+                Text("This is a bio about all the great things she has done with monkeys and being a pioneering women and such!!!")
+                
+                
+            }
+            .padding()
+            
+            
+            .background(Rectangle() .foregroundColor(.white))
+            .cornerRadius(15)
+            .shadow(radius: 15)
+            .padding()
+            
+            
+        }
+        
+        
+        
+        
+        
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
